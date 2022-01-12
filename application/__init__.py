fcf825440d0fc1f2d01debb804c999493bc3770d
like-ycy <15210198546@163.com>
2022-01-12 20:04:23 +0800

    配置redis连接

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 """项目初始化主程序"""
 from flask import Flask
+from flask_redis import FlaskRedis
 from flask_sqlalchemy import SQLAlchemy
 
 from application.utils.config import Config
@@ -8,12 +9,18 @@ from application.utils.config import Config
 config: Config = Config()
 
 # 初始化SqlAlchemy
-db: SQLAlchemy = SQLAlchemy()
+mysqldb: SQLAlchemy = SQLAlchemy()
+
+# Redis初始化
+redis_cache: FlaskRedis = FlaskRedis(config_prefix='REDIS')
+redis_check: FlaskRedis = FlaskRedis(config_prefix='CHECK')
 
 
 def init_app(config_path) -> Flask:
     """初始化Flask应用"""
     app: Flask = Flask(__name__)
     config.init_app(app, config_path)
-    db.init_app(app)
+    mysqldb.init_app(app)
+    redis_cache.init_app(app)
+    redis_check.init_app(app)
     return app
--- application/settings/dev.py
+++ application/settings/dev.py
@@ -11,3 +11,9 @@ SQLALCHEMY_DATABASE_URI: str = "mysql://mofang_user:mofang@127.0.0.1:3306/mofang
 
 # 查询时会显示原始SQL语句
 SQLALCHEMY_ECHO: bool = True
+
+"""Redis数据库配置"""
+# 默认缓存数据
+REDIS_URL: str = "redis://dba:123.com@127.0.0.1:6379/0"
+# 验证相关缓存
+CHECK_URL: str = "redis://dba:123.com@127.0.0.1:6379/1"
