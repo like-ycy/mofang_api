d411d0051ce692c54fd0990964c39362ab88f9bb
like-ycy <15210198546@163.com>
2022-01-19 20:31:47 +0800

    错误信息与错误码

---
+++ application/utils/code.py
@@ -0,0 +1,2 @@
+CODE_OK: int = 0  # 接口操作成功
+CODE_VALIDATE_ERROR: int = 1001  # 验证有误！
---
+++ application/utils/message.py
@@ -0,0 +1,4 @@
+OK: str = "success"
+MOBILE_FORMAT_ERROR: str = "手机号格式有误！"
+MOBILE_IS_EMPTY: str = "手机号不能为空！"
+MOBILE_EXISTS: str = "手机号已经被注册！"
