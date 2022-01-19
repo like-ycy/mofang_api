751c7acc94a6efafb7de0187409070251e0dc1f7
like-ycy <15210198546@163.com>
2022-01-19 17:40:53 +0800

    删除无用子应用

--- application/apps/home/urls.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from application import path
-from . import views
-
-urlpatterns: List = [
-    path("/index", views.index, methods=['GET']),
-    path("/test", views.test, methods=['GET']),
-]
--- application/apps/home/views.py
+++
@@ -1,6 +0,0 @@
-def index():
-    return 'Hello World!'
-
-
-def test():
-    return 'test  test  test!'
