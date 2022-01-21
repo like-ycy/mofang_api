9895cff823cb362fc6c073c1e2829b74e3e31eeb
like-ycy <15210198546@163.com>
2022-01-21 10:21:06 +0800

    添加一个函数使用导包，不然代码格式化celery导包被删除，shell无法启动celery

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
+from application import celery
 from application import init_app, Flask
 
 app: Flask = init_app("application.settings.dev")
 
+
+@celery.task(name="send_sms")
+def send_sms(mobile: str):
+    """占用celery导包，不然代码格式化把导包删除了，shell启动不了celery"""
+    print("发送短信~")
+
+
 if __name__ == '__main__':
     app.run()
+    send_sms.delay("18888888888")
