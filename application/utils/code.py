a63cd19a95e944d08f824d8f776c355e72676f6d
like-ycy <15210198546@163.com>
2022-01-24 11:18:48 +0800

    修改jwt_extended源码，针对token验证失败、过期、无效的错误提示，调整返回的数据格式和状态码

--- application/utils/code.py
+++ application/utils/code.py
@@ -1,5 +1,8 @@
-CODE_OK: int = 0  # 接口操作成功
-CODE_VALIDATE_ERROR: int = 1001  # 验证有误！
-CODE_INTERVAL_TIME: int = 1002  # 接口操作过于频繁!
-CODE_SMS_ERROR: int = 1100  # 短信发送失败！
-CODE_USER_NOT_EXISTS: int = 1101  # 当前用户不存在！
+CODE_OK = 0  # 接口操作成功
+CODE_VALIDATE_ERROR = 1001  # 验证有误！
+CODE_INTERVAL_TIME = 1002  # 接口操作过于频繁!
+CODE_SMS_ERROR = 1100  # 短信发送失败！
+CODE_USER_NOT_EXISTS = 1101  # 当前用户不存在！
+CODE_JWT_INVALID = 1010  # 无效的jwt token
+CODE_JWT_NOT_TOKEN = 1011  # 没有找到jwt token
+CODE_JWT_EXPIRED = 1012  # jwt登录已超时
